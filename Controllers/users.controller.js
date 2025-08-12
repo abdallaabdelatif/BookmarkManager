d54e578f7919830a8f3dd3098849e145d1b2c148
Mahmoudhazemm <mahmoudhazem62004@gmail.com>
2025-08-12 03:43:38 +0300

    hashing the password in model.js (pre-save method)

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -4,25 +4,15 @@ const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// Register
 exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    // Check if user exists
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create user
-    const newUser = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
+    const newUser = await User.create({ username, email, password });
 
     res.status(201).json({ message: "User registered", user: newUser });
   } catch (err) {
@@ -30,22 +20,17 @@ exports.register = async (req, res) => {
   }
 };
 
-// Login
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
   try {
-    // Find user
-    const user = await User.findOne({ email: email });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-    // Check password
-    let match = await bcrypt.compare(password, user.password);
+    const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(400).json({ message: "Invalid credentials" });
 
-    // Create token
-    let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "7d" });
 
     res.json({ message: "Login successful", token });
   } catch (err) {
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const userRoutes = require("./Routes/users.route");
 const bookmarkRoutes = require("./Routes/bookmarks.route");
 
 const app = express();
 
-// Built-in middlewares
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 app.use("/users", userRoutes);
 app.use("/bookmarks", bookmarkRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to the DB Successfully."))
-  .catch((error) => console.log("Connection Failed."));
+  .catch((error) => console.log("Connection Failed.",error));
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
   console.log(`Server started listening on port: => ${port}`);
 });
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
